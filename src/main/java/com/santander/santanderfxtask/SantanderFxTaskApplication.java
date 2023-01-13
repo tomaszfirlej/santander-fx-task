75ff52db2f5567514226c3c5d110a79e2e8583cb
Tomek <tfdeveloper7@gmail.com>
2023-01-13 12:45:00 +0100

    first

---
+++ src/main/java/com/santander/santanderfxtask/SantanderFxTaskApplication.java
@@ -0,0 +1,13 @@
+package com.santander.santanderfxtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderFxTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SantanderFxTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/santander/santanderfxtask/SantanderFxTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santander.santanderfxtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderFxTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
